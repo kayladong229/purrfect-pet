5b52cc5fda13e104d430efc9005a13999c10a584
Shawn Chang <shawnchang@shawnchang-m62.lan>
2022-11-21 20:27:58 -0800

    comment out service worker for now

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,4 +12,4 @@ root.render(
   </React.StrictMode>
 );
 
-serviceWorker.register();
+// serviceWorker.register();
