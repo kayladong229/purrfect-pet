af306e6cc53f8f15a7fe5e63cf08354482f2ebd2
Shawn Chang <shawnchang@shawnchang-m62.lan>
2022-11-21 20:13:58 -0800

    apollo and authorization to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,50 @@
 import React from 'react';
 import './App.css';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+
+//import authorization
+import auth from './utils/auth';
+
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    <div className='App'>
+      <header className='App-header'>
+        {/* <img src={logo} className='App-logo' alt='logo' /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit Hello one two three<code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
