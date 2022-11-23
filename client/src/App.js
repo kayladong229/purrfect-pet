527c7f236189d8f90d9f47645fc48ce7541b47f8
Jose San Mateo <info@joseasanmateo.com>
2022-11-22 19:13:29 -0800

    Added some React stuff.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import './App.css';
+import Banner from './utils/banner';
+import NavBar from './utils/NavBar'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <><Banner /><NavBar /></>
+  )
+};
 
 export default App;
