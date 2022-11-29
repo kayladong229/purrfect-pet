225df0067bef17b810557b21c2676c0b9193932c
Shawn Chang <shawnchang@shawnchang-m62.lan>
2022-11-29 15:22:54 -0800

    searchpets works

--- client/src/pages/SearchPets.js
+++ client/src/pages/SearchPets.js
@@ -18,7 +18,6 @@ import { SAVE_PET } from "../utils/mutations";
 import { useContext } from "react";
 import { AuthContext } from "../App";
 
-
 const SearchPets = () => {
   const accessToken = useContext(AuthContext);
   // create state for holding returned Petfinder api data
@@ -48,108 +47,137 @@ const SearchPets = () => {
     try {
       const response = await fetchPets(searchInput, accessToken);
       console.log(response)
-      setsearchedPets(await response.json());
       if (!response.ok) {
         throw new Error("something went wrong!");
       }
-      
-      // const animals = await response.json();
-      console.log(searchedPets)
-      // const petData = animals.map((animal) => { return ( {
-      //   petId: animal.id,
-      //   type: animal.type,
-      //   gender: animal.gender,
 
-      // })});
-// console.log(petData)
+      const matchPets = await response.json();
+      var photo = matchPets?.animals?.[0]?.photos?.[0]?.medium
+      if (photo) { 
+          console.log(photo); 
+      }
+          else { console.log('photo is empty'); 
+      }
+          
+      console.log(matchPets?.animals?.[0])
+
+
+      const petData = matchPets.animals.map((animals) => ({
+        petId: animals.id,
+        type: animals.type,
+        name: animals.name,
+        breed: animals.breed,
+        status: animals.status,
+        gender: animals.gender,
+        description: animals.description,
+        image: animals?.[0]?.photos?.[0]?.medium || "",
+      }));
+      setsearchedPets(petData);
       setSearchInput("");
     } catch (err) {
       console.error(err);
     }
   };
-console.log(searchedPets.animals)
+
   // create function to handle saving a pet to our database
-  // const handleSavePet = async (petId) => {
-  //   // find the pet in `searchedPets` state by the matching id
-  //   const petToSave = searchedPets.find((searchedPets) => animal.petId === petId);
+  const handleSavePet = async (petId) => {
+    // find the pet in `searchedPets` state by the matching id
+    const petToSave = searchedPets.find((animals) => animals.petId === petId);
 
-  //   // get token
-  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-  //   if (!token) {
-  //     return false;
-  //   }
+    if (!token) {
+      return false;
+    }
 
-  //   try {
-  //     const { data } = await savePet({
-  //       variables: { input: petToSave },
-  //     });
+    try {
+      const { data } = await savePet({
+        variables: { input: petToSave },
+      });
 
-  //     // if pet successfully saves to user's account, save pet id to state
-  //     setSavedPetIds([...savedPetIds, petToSave.petId]);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
+      // if pet successfully saves to user's account, save pet id to state
+      setSavedPetIds([...savedPetIds, petToSave.petId]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 console.log(searchInput);
 const handleInputChange = (e) => {
   setSearchInput(e.target.value)
 }  
 return (
-<>
-  <Container>
-    <Jumbotron fluid className="text-light bg-dark">
-      <h1>Search for Pets!</h1>
-        <Form onSubmit={handleFormSubmit}>
-          <Form.Row>
-            <Col xs={12} md={8}>
-              <Form.Control
-                name="searchInput"
-                value={searchInput}
-                onChange={handleInputChange}
-                type="text"
-                size="lg"
-                placeholder="Search for a pet"
-              />
-            </Col>
-            <Col xs={12} md={4}>
-              <Button type="submit" variant="success" size="lg">
-                Submit Search
-              </Button>
-            </Col>
-          </Form.Row>
-        </Form>
-    </Jumbotron>
+    <>
+      <Jumbotron fluid className="text-light bg-dark">
+        <Container>
+          <h1>Search for Pets!</h1>
+          <Form onSubmit={handleFormSubmit}>
+            <Form.Row>
+              <Col xs={12} md={8}>
+                <Form.Control
+                  name="searchInput"
+                  value={searchInput}
+                  onChange={handleInputChange}
+                  type="text"
+                  size="lg"
+                  placeholder="Search for a pet"
+                />
+              </Col>
+              <Col xs={12} md={4}>
+                <Button type="submit" variant="success" size="lg">
+                  Submit Search
+                </Button>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Container>
+      </Jumbotron>
 
+      <Container>
         <h2>
           {searchedPets.length
             ? `Viewing ${searchedPets.length} results:`
             : "Search for a pet to begin"}
         </h2>
         <CardColumns>
-          return ( 
-            {
-              searchedPets.map((animal, index) => {
-                <Card key={animal.petId} border="dark">
-                  {animal.photos ? (
-                    <Card.Img
-                      src={animal[index].photos[0].medium}
-                      alt={`I am ${animal.status}`}
-                      variant="top"
-                    />
-                  ) : <div>No Image Found </div>}
-                  <Card.Body>
-                    <Card.Title>{animal.type}</Card.Title>
-                    <p className="small">Pet type: {animal.type}</p>
-                    <Card.Text>{animal.description}</Card.Text>
-                  </Card.Body>
-                </Card>
-              })
-            }
-        );
+          {searchedPets.map((animals) => {
+            return (
+              <Card key={animals.petId} border="dark">
+                {animals?.photos ? (
+                  <Card.Img
+                    src={animals?.photos}
+                    alt={`I am ${animals.type}`}
+                    variant="top"
+                  />
+                ) : null}
+                <Card.Body>
+                  <Card.Img>{animals?.photos}</Card.Img>
+                  <Card.Title>{animals.name}</Card.Title>
+                  <p className="small">gender: {animals.gender}</p>
+                  <Card.Text>{animals.description}</Card.Text>
+                  {Auth.loggedIn() && (
+                    <Button
+                      disabled={savedPetIds?.some(
+                        (savedPetId) => savedPetId === animals.petId
+                      )}
+                      className="btn-block btn-info"
+                      onClick={() => handleSavePet(animals.petId)}
+                    >
+                      {savedPetIds?.some(
+                        (savedPetId) => savedPetId === animals.petId
+                      )
+                        ? "This pet has already been saved!"
+                        : "Save this pet!"}
+                    </Button>
+                  )}
+                </Card.Body>
+              </Card>
+            );
+          })}
         </CardColumns>
-  </Container>
-</>
+      </Container>
+    </>
   );
-}
+};
+
 export default SearchPets;
